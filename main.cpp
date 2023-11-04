c5dd8899b26334d6d5ff1cbefa5c80097778cf79
Ju Hyunsahng <hsju2013@gmail.com>
2023-11-04 21:37:41 +0900

    revert to c3a6c2

--- main.cpp
+++ main.cpp
@@ -11,7 +11,39 @@
 
 using namespace std;
 
-
+string msg[] = {
+    ">>> ",
+    "메뉴를 선택하세요.\n",
+    "원하는 포진을 선택하세요.\n",
+    "{player}나라의 기물을 선택하세요.\n",
+    "{player}나라가 게임을 중단하여 메인 메뉴로 돌아갑니다.\n",
+    "‘y’또는 ‘Y’를 입력해주세요.\n",
+    "{player}나라의 기물을 선택하세요.\n",
+    "{player}나라 ‘{piece}’을(를) 선택하셨습니다.\n",
+    "기물을 다시 선택하려면 “back”을 입력하세요.\n",
+    "게임을 중단하려면 “quit”을 입력하세요.\n",
+    "이동할 좌표를 입력해주세요.\n",
+    "{lose_player}나라의 궁이 잡혔습니다.\n",
+    "{win_player}나라의 승리입니다!\n",
+    "‘y’또는 ‘Y’를 입력하면 메인 메뉴로 돌아갑니다.\n",
+    "100번의 턴을 진행하여 점수 합산으로 승패를 결정합니다.\n",
+    "양측 기물의 점수 총합이 30 이하이므로 점수 합산으로 승패를 결정합니다.\n",
+    "점수 총합 : 초나라 {score}점, 한나라 {score}점\n",
+    "한나라가 {setup} 포진을 선택하였습니다.\n",
+    "{player}나라 ‘{piece}’이(가) {player}나라 ‘{piece}’을(를) 잡았습니다.\n ",
+    "존재하지 않는 메뉴입니다. ",
+    "다시 입력해 주세요.\n",
+    "해당 좌표에 기물이 존재하지 않거나 옳지 않은 입력입니다. ",
+    "불가능한 이동입니다. ",
+    "해당되는 포진이 없습니다. 1과 4 사이의 정수를 입력하세요.\n",
+    "잘못된 입력입니다. ‘y’또는 ‘Y’를 입력하세요.\n",
+    "상대 플레이어의 기물은 선택할 수 없습니다. ",
+    "초나라는 한나라에서 제거할 기물의 수를 입력하세요. (0~6)\n",
+    "{num}()를 입력받았습니다.\n",
+    "{player}는 포진 선택과정을 진행하세요.\n",
+    "한나라는 제거할 {num}()개의 좌표를 입력하세요.\n",
+    "접장기 규칙에 따라 한나라의 선공으로 진행합니다.\n"
+};
 class Player {
    public:
     int score;
@@ -74,11 +106,26 @@ void Piece::movePiece(int x, int y) {
     vector<pair<int, int>> paths = generatePaths();
     while(1) {
         for (int i = 0; i < paths.size(); i++) {
-            if (paths[i].first == x && paths[i].second == y) {
-                board[x][y] = board[this->x][this->y];
-                board[this->x][this->y] = nullptr;
-                this->x = x;
-                this->y = y;
+            cout << paths[i].second << (char)(paths[i].first + 'A') << ' ';
+        }
+        cout << '\n';
+        cout << "어느 좌표로 이동하시겠습니까?" << endl << ">>>";
+        getline(cin, coord);
+
+        // 좌표 입력 규칙 확인 (2글자이고, 첫번째는 숫자이고, 두번째는 소문자 혹은 대문자인지)
+        if (coord.length() !=2 || !isdigit(coord[0]) || (!(coord[1] >= 'a' && coord[1] <= 'i') && !(coord[1] >= 'A' && coord[1] <= 'I')))  {
+            cout << msg[21] << msg[20] << endl;
+            // continue;
+        }
+
+        // 규칙에 맞을 경우 숫자로 변환
+        tmpy = coord[0] - '0';
+        tmpx = (coord[1] >= 'a' && coord[1] <= 'i') ? coord[1] - 'a' : coord[1] - 'A';
+
+        for (int i = 0; i < paths.size(); i++) {
+            if (paths[i].first == tmpx && paths[i].second == tmpy) {
+                board[tmpx][tmpy] = board[x][y];
+                board[x][y] = nullptr;
                 return;
             }
         }
@@ -113,6 +160,17 @@ int isMovable(int x, int y, char team) {
     else return 2;
 }
 
+void kill() {
+
+}
+
+void undo() {
+
+}
+
+bool choCheckWin() {
+    return false;
+}
 
 
 // derived class (Rook, Cannon, Knight, Elephant, King, Guard, Pawn)
