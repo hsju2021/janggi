5394cb1baf23d36b6d76aab63cda7d68b0a16a2a
codinghuman0 <codinghuman0@gmail.com>
2023-11-06 02:23:33 +0900

    added chcp 65001

--- main.cpp
+++ main.cpp
@@ -784,6 +784,8 @@ Piece* quit = new Pawn(1, 1, '.');
 Piece* cancel = new Pawn(1, 1, ',');
 
 int main() {
+    // 컴파일 시 한글 깨짐 해결
+    system("chcp 65001");
     int remove;
     string input;
     Piece* chosen;
